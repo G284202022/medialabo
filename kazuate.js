b3d131b5b7600efb6df33befa2bf3573a6ab5c61
G284202022 <228420@st.takushoku-u.ac.jp>
2023-05-25 00:08:17 +0900

    resultに出力するよう修正

--- kazuate.js
+++ kazuate.js
@@ -33,16 +33,40 @@ function hantei() {
   if (kaisu < 3) {
     if (kotae === yoso) {
       console.log("正解です．おめでとう!");
+      let a = document.querySelector('p#result');
+      let a1 = document.createElement('p');
+      a1.textContent = '正解です.おめでとう!';
+      a.insertAdjacentElement('beforeend', a1);
     } else if (kotae > yoso) {
       console.log("まちがい．答えはもっと大きいですよ");
+      let b = document.querySelector('p#result');
+      let b1 = document.createElement('p');
+      b1.textContent = 'まちがい.答えはもっと大きいですよ';
+      b.insertAdjacentElement('beforeend', b1);
     } else if (kotae < yoso) {
       console.log("まちがい．答えはもっと小さいですよ");
+      let c = document.querySelector('p#result');
+      let c1 = document.createElement('p');
+      c1.textContent = 'まちがい．答えはもっと小さいですよ';
+      c.insertAdjacentElement('beforeend', c1);
     }  
   } else if (kaisu === 3 && kotae !== yoso) {
     console.log("まちがい．残念でした答えは" + kotae + "です．");
+    let d = document.querySelector('p#result');
+    let d1 = document.createElement('p');
+    d1.textContent = 'まちがい．残念でした答えは'+kotae+'です.';
+    d.insertAdjacentElement('beforeend', d1);
   } else if (kaisu === 3 && kotae === yoso) {
     console.log("正解です．おめでとう!");
+    let e = document.querySelector('p#result');
+    let e1 = document.createElement('p');
+    e1.textContent = '正解です.おめでとう！';
+    e.insertAdjacentElement('beforeend', e1);
   } else if (kaisu > 3) {
     console.log("答えは" + kotae + "でした．すでにゲームは終わっています");
+    let f = document.querySelector('p#result');
+    let f1 = document.createElement('p');
+    f1.textContent = '答えは'+kotae+'でした．すでにゲームは終わっています';
+    f.insertAdjacentElement('beforeend', f1);
   }
 }
