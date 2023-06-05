0e974e38f5db860d0b1dfbef974a66c84057d16e
G284202022 <228420@st.takushoku-u.ac.jp>
2023-06-05 12:39:41 +0900

    エラーを修正

--- project.js
+++ project.js
@@ -61,7 +61,7 @@ function showResult(resp) {
             
             var s = [a.titile,a.subtitle,a.start_name,a.end_time,a.content,a.act];
             for (let i = 0;i<6;i++){
-                let tr = document.querySelector('tbody.rows['+table.rows.length-1+']');
+                let tr = document.querySelector('tbody.rows['+tbody.rows.length-1+']');
                 let j = document.createElement('td');
                 j.textContent = s[i];
                 tr.insertAdjacentElement('beforeend', j);
@@ -76,7 +76,7 @@ function showResult(resp) {
             
             var s = [a.titile,a.subtitle,a.start_name,a.end_time,a.content,a.act];
             for (let i = 0;i<6;i++){
-                let tr = document.querySelector('tbody.rows['+table.rows.length-1+']');
+                let tr = document.querySelector('tbody.rows['+tbody.rows.length-1+']');
                 let j = document.createElement('td');
                 j.textContent = s[i];
                 tr.insertAdjacentElement('beforeend', j);
